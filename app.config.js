7540003e2503e58520f36ed54db00753f232ce07
csmeby <connorsmeby@tamu.edu>
2026-08-22 15:41:36 -0500

    version BUMP.

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ module.exports = {
   expo: {
     name: "Century Tree Transit",
     slug: "bus",
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "ctt",
@@ -16,7 +16,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.csmeby.ctt",
-      buildNumber: "5",
+      buildNumber: "1",
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSAppTransportSecurity: {
